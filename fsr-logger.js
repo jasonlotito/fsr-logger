226988576a2004d7bc726bf9b4d80ecbf36a9135
Jason Lotito <jlotito@meetme.com>
2014-01-12 11:24:46 -0500

    Updated docblock comments, by update, I mean add

--- fsr-logger.js
+++ fsr-logger.js
@@ -2,7 +2,15 @@ var
   moment = require('moment'),
   fsr = require('file-stream-rotator');
 
-function create_simple_config (name, frequency, verbosity) {
+/**
+ * Neatly abstracts out the creation of file-stream-rotator configs
+ *
+ * @param {string} name
+ * @param {string} frequency
+ * @param {boolean} verbosity
+ * @returns {{}}
+ */
+function create_fsr_config (name, frequency, verbosity) {
   var config = {}
   config.filename = name || './log/activity.log';
   config.frequency = frequency || 'daily';
@@ -10,15 +18,32 @@ function create_simple_config (name, frequency, verbosity) {
   return config;
 }
 
+/**
+ * Creates a logger function based on the config
+ *
+ * Default config is:
+ *
+ * filename: ./log/activity.log
+ * frequency: daily
+ * verbose: false
+ *
+ * Returns a function that can be used to add messages to the logger
+ *
+ * @param {{filename: string, frequency: string, verbose: boolean}} streamConfig Config to create the file stream
+ * @returns {function(string)}
+ */
 function create_logger (streamConfig) {
+  var conf;
 
   if (! streamConfig) {
-    streamConfig = create_simple_config();
+    conf = create_fsr_config();
   } else if (typeof streamConfig == 'string') {
-    streamConfig = create_simple_config(streamConfig);
+    conf = create_fsr_config(streamConfig);
+  } else {
+    conf = streamConfig;
   }
 
-  var stream = fsr.getStream(streamConfig);
+  var stream = fsr.getStream(conf);
 
   return function(msg) {
     stream.write(moment().toISOString() + ' ' + msg);
@@ -26,5 +51,6 @@ function create_logger (streamConfig) {
 }
 
 module.exports = {
-  create_logger: create_logger
+  create_logger: create_logger,
+  create_fsr_config: create_fsr_config
 };
